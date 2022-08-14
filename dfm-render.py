24ba6cfc141a565592e1896acdcb560dd532d077
Tom <tom@msbit.com.au>
2022-08-15 01:18:51 +1000

    Formatting with autopep8

--- dfm-render.py
+++ dfm-render.py
@@ -5,102 +5,115 @@ from PIL import Image, ImageDraw
 from re import compile
 from sys import argv, exit
 
+
 def build_frame(groups):
-  return {
-    'name': groups[0],
-    'type': groups[1],
-    'attributes': {},
-    'children': []
-  }
+    return {
+        'name': groups[0],
+        'type': groups[1],
+        'attributes': {},
+        'children': []
+    }
+
 
 def build_structure(filename):
-  open_exp = compile('^[ ]*object (.*): (.*)\n$')
-  close_exp = compile('^[ ]*end\n$')
-  attribute_exp = compile('^[ ]*(.*) = (.*)\n$')
+    open_exp = compile('^[ ]*object (.*): (.*)\n$')
+    close_exp = compile('^[ ]*end\n$')
+    attribute_exp = compile('^[ ]*(.*) = (.*)\n$')
 
-  with open(filename, 'r') as f:
-    lines = f.readlines()
+    with open(filename, 'r') as f:
+        lines = f.readlines()
 
-    stack = [{'children': []}]
+        stack = [{'children': []}]
 
-    for line in lines:
-      open_match = open_exp.match(line)
-      if open_match:
-        child = build_frame(open_match.groups())
-        stack[-1]['children'].append(child)
-        stack.append(child)
-        continue
+        for line in lines:
+            open_match = open_exp.match(line)
+            if open_match:
+                child = build_frame(open_match.groups())
+                stack[-1]['children'].append(child)
+                stack.append(child)
+                continue
 
-      close_match = close_exp.match(line)
-      if close_match:
-        stack.pop()
-        continue
+            close_match = close_exp.match(line)
+            if close_match:
+                stack.pop()
+                continue
 
-      attribute_match = attribute_exp.match(line)
-      if attribute_match:
-        groups = attribute_match.groups()
-        stack[-1]['attributes'][groups[0]] = groups[1]
-        continue
+            attribute_match = attribute_exp.match(line)
+            if attribute_match:
+                groups = attribute_match.groups()
+                stack[-1]['attributes'][groups[0]] = groups[1]
+                continue
 
-  return stack[0]['children'][0]
+    return stack[0]['children'][0]
+
 
 def first_in(d, *args):
-  for arg in args:
-    if arg in d: return d[arg]
+    for arg in args:
+        if arg in d:
+            return d[arg]
 
-  return None
+    return None
+
 
 def extract_dimensions(attributes):
-  left = attributes.get('Left')
-  if left == None: return None
+    left = attributes.get('Left')
+    if left == None:
+        return None
 
-  top = attributes.get('Top')
-  if top == None: return None
+    top = attributes.get('Top')
+    if top == None:
+        return None
 
-  width = first_in(attributes, 'ClientWidth', 'Width')
-  if width == None: return None
+    width = first_in(attributes, 'ClientWidth', 'Width')
+    if width == None:
+        return None
 
-  height = first_in(attributes, 'ClientHeight', 'Height')
-  if height == None: return None
+    height = first_in(attributes, 'ClientHeight', 'Height')
+    if height == None:
+        return None
 
-  return [int(left), int(top), int(left) + int(width), int(top) + int(height)]
+    return [int(left), int(top), int(left) + int(width), int(top) + int(height)]
+
 
 def plumb(node, depth=0, breadth=0):
-  depth += 1
+    depth += 1
 
-  if len(node['children']) == 0: return (depth, breadth)
+    if len(node['children']) == 0:
+        return (depth, breadth)
 
-  breadth = max(breadth, len(node['children']))
-  depth = max(plumb(c, depth, breadth)[0] for c in node['children'])
+    breadth = max(breadth, len(node['children']))
+    depth = max(plumb(c, depth, breadth)[0] for c in node['children'])
 
-  return (depth, breadth)
+    return (depth, breadth)
+
 
 def render(node):
-  max_depth, max_breadth = plumb(node)
-  hue_coarse = 360.0 / float(max_depth + 1)
-  hue_fine = hue_coarse / float(max_breadth + 1)
+    max_depth, max_breadth = plumb(node)
+    hue_coarse = 360.0 / float(max_depth + 1)
+    hue_fine = hue_coarse / float(max_breadth + 1)
 
-  def render_recursive(node, draw, depth=0, index=0):
-    hidden = node['attributes'].get('Visible') == 'False'
-    dimensions = extract_dimensions(node['attributes'])
-    if dimensions and not hidden:
-      hue = ((hue_coarse * depth) + (hue_fine * index)) / 360.0
-      rgb = [int(x * 255.0) for x in hsv_to_rgb(hue, 1.0, 0.8)]
-      draw.rectangle(dimensions, fill=tuple(rgb), outline=(0, 0, 0))
+    def render_recursive(node, draw, depth=0, index=0):
+        hidden = node['attributes'].get('Visible') == 'False'
+        dimensions = extract_dimensions(node['attributes'])
+        if dimensions and not hidden:
+            hue = ((hue_coarse * depth) + (hue_fine * index)) / 360.0
+            rgb = [int(x * 255.0) for x in hsv_to_rgb(hue, 1.0, 0.8)]
+            draw.rectangle(dimensions, fill=tuple(rgb), outline=(0, 0, 0))
 
-    for index, child in enumerate(node['children']):
-      render_recursive(child, draw, depth + 1, index)
+        for index, child in enumerate(node['children']):
+            render_recursive(child, draw, depth + 1, index)
 
-  width = int(node['attributes']['ClientWidth'])
-  height = int(node['attributes']['ClientHeight'])
-  image = Image.new('RGB', (width, height), (255, 255, 255))
-  draw = ImageDraw.Draw(image)
-  render_recursive(node, draw)
-  return image
+    width = int(node['attributes']['ClientWidth'])
+    height = int(node['attributes']['ClientHeight'])
+    image = Image.new('RGB', (width, height), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    render_recursive(node, draw)
+    return image
+
 
 if len(argv) < 3:
-  print(f'usage: {argv[0]} <dfm-file> <png-file>')
-  exit(1)
+    print(f'usage: {argv[0]} <dfm-file> <png-file>')
+    exit(1)
 
 structure = build_structure(argv[1])
 
